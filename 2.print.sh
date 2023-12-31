164aa7e1f586d7830aa66a2be423e60ee9e6fa17
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2023-12-31 15:53:51 +0530

    Adding colours to messages

--- 2.print.sh
+++ 2.print.sh
@@ -1,4 +1,4 @@
-echo Hello world
+echo -e "\e[36m Hello world
 
 ## Any line starting with hash is a comment in Shell Scripting
 
@@ -11,15 +11,15 @@ echo Hello world
 # MAGENTA - 35
 # CYAN  - 36
 
-#Syntax : echo -e "\e[COLCODEmMessage\e[0m"
+#Syntax : echo -e "\e[36m -e "\e[COLCODEmMessage\e[0m"
 
 # -e is to enable \e
 # \e[COLCODEm -> \e[31m this is to enable the clour
 #\e[0m - > this is to disable the code
 
-echo -e "\e[31mHello World\e[0m"
-echo -e "\e[32mHello World\e[0m"
-echo -e "\e[33mHello World\e[0m"
-echo -e "\e[34mHello World\e[0m"
-echo -e "\e[35mHello World\e[0m"
-echo -e "\e[36mHello World\e[0m"
+echo -e "\e[36m -e "\e[31mHello World\e[0m"
+echo -e "\e[36m -e "\e[32mHello World\e[0m"
+echo -e "\e[36m -e "\e[33mHello World\e[0m"
+echo -e "\e[36m -e "\e[34mHello World\e[0m"
+echo -e "\e[36m -e "\e[35mHello World\e[0m"
+echo -e "\e[36m -e "\e[36mHello World\e[0m"
