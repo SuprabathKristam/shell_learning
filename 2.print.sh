8c56ae37985d2aac340f4b7ae4d2ba39fe0dad25
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2023-12-31 15:46:36 +0530

    Adding colours to messages

--- 2.print.sh
+++ 2.print.sh
@@ -1,1 +1,25 @@
 echo Hello world
+
+## Any line starting with hash is a comment in Shell Scripting
+
+## Colour Concept we will have 6 colours in Shell
+
+# RED - 31
+# GREEN - 32
+# YELLOW - 33
+# BLUE - 34
+# MAGENTA - 35
+# CYAN  - 36
+
+#Syntax : echo -e "\e[COLCODEmMessage\e[0m"
+
+# -e is to enable \e
+# \e[COLCODEm -> \e[31m this is to enable the clour
+#\e[0m - > this is to disable the code
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
