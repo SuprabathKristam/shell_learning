414c896c4ef88332e448a7d1f248d3c890103429
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-01 18:53:15 +0530

    Learning special variables

--- 3.Special-variables.sh
+++ 3.Special-variables.sh
@@ -1,8 +1,8 @@
 #Special variables are 0,1,2,3,4……..n , * , #  these are the special variables
 
-echo The name of the Script is $0 # this gives the name of the script with in the script
-echo first arugument is $1 # This gives first argument
-echo second argument is $2 # This gives the second argument
-echo All Arguments $*  # This will give all arguments
-echo Total number of arguments $# # This will give the count of total number of arguments
+echo The name of the Script is: $0 # this gives the name of the script with in the script
+echo first arugument is: $1 # This gives first argument
+echo second argument is: $2 # This gives the second argument
+echo All Arguments: $*  # This will give all arguments
+echo Total number of arguments:  $# # This will give the count of total number of arguments
 
