3e6bab5ef001b53e6b96410153d16b519d4c6a3d
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-01 18:19:36 +0530

    Learning special variables

--- 3.Special-variables.sh
+++ 3.Special-variables.sh
@@ -1,6 +1,6 @@
 #Special variables are 0,1,2,3,4……..n , * , #  these are the special variables
 
-echo $0 # this gives the name of the script with in the script
+echo The name of the Script is $0 # this gives the name of the script with in the script
 echo $1
 echo $2
 
