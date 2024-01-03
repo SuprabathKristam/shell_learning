102ca07d232967e3c1491a95c7d51934dc4c0809
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-03 15:20:50 +0530

    Learning functions

--- 4.functions.sh
+++ 4.functions.sh
@@ -1,5 +1,19 @@
+#Function
 sample(){
+  #Code inside function
   echo "Hello World"
 }
-
+#Main Program
 sample
+
+
+##Declared variable in main program can be accessed in function and vice versa
+##Declared variable in main program can be over written in function and vice versa
+##Along with these variables we have special variables , we can pass arguments to the functions
+
+sample_sum(){
+  echo "Addition of $1 and $2 is : $(($1+$2))"
+}
+
+sample_sum
+
